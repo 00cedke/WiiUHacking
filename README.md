b100ef2ecc59b6558f1b30cf8ab2497c151e6b64
cedkeDev123 <123301168+00cedke@users.noreply.github.com>
2023-09-09 21:32:20 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# WiiUHacking
+Created by cedkeChat And Azaee
+
+# What is it ?
+it's a program that allows to create hacks for the games you want only in C++.
+it's very easy to use if you know the basics of code in C++ and to create an .elf/.rpx.
+
+# How to make it ?
+You must install libutils, dynamic_libs and libkernel.
+
+# Credits
+cedkeChat for the Thread Code.
+Azaee for the Kernel Games and Loader.
--- build/thread.d
+++ build/thread.d
@@ -1,47 +1,39 @@
-thread.o: C:/Users/cedke/Desktop/WiiUThreadCpp/src/thread.cpp \
- C:/Users/cedke/Desktop/WiiUThreadCpp/src/libs.hpp \
+thread.o: C:/Users/cedke/Desktop/WiiUHacking/src/thread.cpp \
+ C:/Users/cedke/Desktop/WiiUHacking/src/common.h \
  C:/devkitPro/libogc/include/gctypes.h \
- C:/devkitPro/portlibs/ppc/include/dynamic_libs/os_functions.h \
+ C:/devkitPro/portlibs/ppc/include/libutils/utils/logger.h \
+ C:/devkitPro/portlibs/ppc/include/libutils/system/memory.h \
+ C:/devkitPro/portlibs/ppc/include/libutils/kernel/syscalls.h \
+ C:/devkitPro/portlibs/ppc/include/libutils/kernel/kernel_defs.h \
+ C:/devkitPro/portlibs/ppc/include/dynamic_libs/fs_functions.h \
  C:/devkitPro/portlibs/ppc/include/dynamic_libs/os_types.h \
+ C:/devkitPro/portlibs/ppc/include/dynamic_libs/fs_defs.h \
+ C:/devkitPro/portlibs/ppc/include/dynamic_libs/os_types.h \
+ C:/devkitPro/portlibs/ppc/include/libutils/kernel/kernel_utils.h \
+ C:/devkitPro/portlibs/ppc/include/dynamic_libs/os_functions.h \
  C:/devkitPro/portlibs/ppc/include/dynamic_libs/os_defs.h \
  C:/devkitPro/portlibs/ppc/include/dynamic_libs/gx2_functions.h \
  C:/devkitPro/portlibs/ppc/include/dynamic_libs/gx2_types.h \
  C:/devkitPro/portlibs/ppc/include/dynamic_libs/vpad_functions.h \
  C:/devkitPro/portlibs/ppc/include/dynamic_libs/sys_functions.h \
  C:/devkitPro/portlibs/ppc/include/dynamic_libs/socket_functions.h \
- C:/devkitPro/portlibs/ppc/include/libutils/utils/logger.h \
- C:/Users/cedke/Desktop/WiiUThreadCpp/src/kernel.hpp \
- C:/devkitPro/portlibs/ppc/include/kernel/kernel_utils.h \
- C:/devkitPro/portlibs/ppc/include/kernel/kernel_defs.h \
- C:/devkitPro/portlibs/ppc/include/dynamic_libs/fs_functions.h \
- C:/devkitPro/portlibs/ppc/include/dynamic_libs/fs_defs.h \
- C:/devkitPro/portlibs/ppc/include/dynamic_libs/os_types.h \
- C:/devkitPro/portlibs/ppc/include/kernel/syscalls.h \
- C:/devkitPro/portlibs/ppc/include/kernel/kernel_defs.h \
- C:/devkitPro/portlibs/ppc/include/libutils/system/memory.h \
- C:/devkitPro/portlibs/ppc/include/libutils/kernel/syscalls.h \
- C:/devkitPro/portlibs/ppc/include/libutils/kernel/kernel_utils.h \
- C:/Users/cedke/Desktop/WiiUThreadCpp/src/thread.h
-C:/Users/cedke/Desktop/WiiUThreadCpp/src/libs.hpp:
+ C:/Users/cedke/Desktop/WiiUHacking/src/thread.h
+C:/Users/cedke/Desktop/WiiUHacking/src/common.h:
 C:/devkitPro/libogc/include/gctypes.h:
-C:/devkitPro/portlibs/ppc/include/dynamic_libs/os_functions.h:
+C:/devkitPro/portlibs/ppc/include/libutils/utils/logger.h:
+C:/devkitPro/portlibs/ppc/include/libutils/system/memory.h:
+C:/devkitPro/portlibs/ppc/include/libutils/kernel/syscalls.h:
+C:/devkitPro/portlibs/ppc/include/libutils/kernel/kernel_defs.h:
+C:/devkitPro/portlibs/ppc/include/dynamic_libs/fs_functions.h:
 C:/devkitPro/portlibs/ppc/include/dynamic_libs/os_types.h:
+C:/devkitPro/portlibs/ppc/include/dynamic_libs/fs_defs.h:
+C:/devkitPro/portlibs/ppc/include/dynamic_libs/os_types.h:
+C:/devkitPro/portlibs/ppc/include/libutils/kernel/kernel_utils.h:
+C:/devkitPro/portlibs/ppc/include/dynamic_libs/os_functions.h:
 C:/devkitPro/portlibs/ppc/include/dynamic_libs/os_defs.h:
 C:/devkitPro/portlibs/ppc/include/dynamic_libs/gx2_functions.h:
 C:/devkitPro/portlibs/ppc/include/dynamic_libs/gx2_types.h:
 C:/devkitPro/portlibs/ppc/include/dynamic_libs/vpad_functions.h:
 C:/devkitPro/portlibs/ppc/include/dynamic_libs/sys_functions.h:
 C:/devkitPro/portlibs/ppc/include/dynamic_libs/socket_functions.h:
-C:/devkitPro/portlibs/ppc/include/libutils/utils/logger.h:
-C:/Users/cedke/Desktop/WiiUThreadCpp/src/kernel.hpp:
-C:/devkitPro/portlibs/ppc/include/kernel/kernel_utils.h:
-C:/devkitPro/portlibs/ppc/include/kernel/kernel_defs.h:
-C:/devkitPro/portlibs/ppc/include/dynamic_libs/fs_functions.h:
-C:/devkitPro/portlibs/ppc/include/dynamic_libs/fs_defs.h:
-C:/devkitPro/portlibs/ppc/include/dynamic_libs/os_types.h:
-C:/devkitPro/portlibs/ppc/include/kernel/syscalls.h:
-C:/devkitPro/portlibs/ppc/include/kernel/kernel_defs.h:
-C:/devkitPro/portlibs/ppc/include/libutils/system/memory.h:
-C:/devkitPro/portlibs/ppc/include/libutils/kernel/syscalls.h:
-C:/devkitPro/portlibs/ppc/include/libutils/kernel/kernel_utils.h:
-C:/Users/cedke/Desktop/WiiUThreadCpp/src/thread.h:
+C:/Users/cedke/Desktop/WiiUHacking/src/thread.h:
