0ebc2e945885157b92c40e4d01c08654cb51b3ca
cedkeDev123 <123301168+00cedke@users.noreply.github.com>
2023-09-09 12:00:14 +0200

    Add files via upload

---
+++ libs.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <gctypes.h>
+#include <dynamic_libs/os_functions.h>
+#include <dynamic_libs/gx2_functions.h>
+#include <dynamic_libs/vpad_functions.h>
+#include <dynamic_libs/sys_functions.h>
+#include <dynamic_libs/socket_functions.h>
+#include <libutils/utils/logger.h>
+#include <libutils/system/memory.h>
+#include <libutils/kernel/syscalls.h>
+#include <libutils/kernel/kernel_utils.h>
