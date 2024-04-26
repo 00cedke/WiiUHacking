a1b06d5a0df38765a03f06296aabb57e63cefa92
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-04-26 10:27:24 +0200

    Update program.cpp

--- src/program.cpp
+++ src/program.cpp
@@ -2,9 +2,10 @@
 #include "program.h"
 
 int MainThread(int argc, void* argv) {
-    uint64_t TitleID = OSGetTitleID();
-    if(TitleID != 0x00050000101d7500)
+    uint64_t title_id = OSGetTitleID();
+    if (title_id != 0x00050000101d7500)
     {
+	log_printf("error for starting program.");
 	return 0;    
     } 
 
