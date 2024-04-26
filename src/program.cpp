a78f9c558e2ad4bef29f2a5ecd5bbf86811cd26a
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-04-26 10:26:17 +0200

    Update program.cpp

--- src/program.cpp
+++ src/program.cpp
@@ -2,8 +2,11 @@
 #include "program.h"
 
 int MainThread(int argc, void* argv) {
-	uint64_t TitleID = OSGetTitleID();
-	if(TitleID != 0x00050000101d7500) return 0;
+    uint64_t TitleID = OSGetTitleID();
+    if(TitleID != 0x00050000101d7500)
+    {
+	return 0;    
+    } 
 
     return 0;
 }
