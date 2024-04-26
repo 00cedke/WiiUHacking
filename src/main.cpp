d38e8d665cfeeb855b073d92dc6fe4eb82637486
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-04-26 10:29:28 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,13 @@ extern "C" int InstallerThread(int argc, char** argv) {
 	bool ret = OSCreateThread(thread, MainThread, 0, NULL, (uint32_t)stack + 0x1000, 0x1000, 0, ((1 << 2) | 8 | 16));
 	if(!ret)
 	{
+		os_sleep(2);
 		return 0;
 	}
 
+	log_printf("===[WiiUHacking by cedkeChat01]===");
 	OSSetThreadName(thread, "WiiUHackingThread");
 	OSResumeThread(thread);
+
+	return -3;
 }
