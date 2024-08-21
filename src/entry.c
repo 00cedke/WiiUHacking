9f0b7c121bcc729098ac437be2099751b7a0a7ee
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-08-21 21:56:25 +0200

    Add files via upload

---
+++ src/entry.c
@@ -0,0 +1,34 @@
+#include "common.h"
+#include "main.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+uint32_t g_alreadyStarted = 0;
+
+int __entry(int argc, char** argv)
+{
+    InitOSFunctionPointers();
+	InitVPadFunctionPointers();
+	InitGX2FunctionPointers();
+	InitSysFunctionPointers();
+	InitSocketFunctionPointers();
+
+	if(g_alreadyStarted)
+		return 0;
+
+	g_alreadyStarted++;
+
+	init_kernel_syscalls();
+
+	log_init();
+	log_printf("\n\n\n---------------[POWERED BY ANOLYNETWORK]---------------\nWiiUHacking by cedkeChat and Azaee\n");
+    log_printf("Starting Thread..\n\n");
+
+	return ThreadMenu(argc, argv);
+}
---
+++ src/main.h
@@ -0,0 +1,12 @@
+#include "common.h"
+#include "thread.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int ThreadMenu(int argc, char** argv);
+
+#ifdef __cplusplus
+}
+#endif
